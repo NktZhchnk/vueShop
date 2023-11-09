9814f24caa3f6245133630ff58479819e8654c18
niktia <mr.nikita.zhuchenko@gmail.com>
2023-11-09 15:34:37 +0200

    fixed image

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,7 @@ onMounted(async () => {
   // Теперь можно безопасно получить данные
   console.log(store.data[0]);
 });
-const getImagePath = (imageName) => {
-  // Возвращает полный путь к изображению на сервере
-  return `https://eseniabila.com.ua/images/${imageName}`;
 
-}
 const fn = () => {
   console.log(store.data)
 }
@@ -31,11 +27,14 @@ const fn = () => {
       <h1>id: {{ item.id }}</h1>
       <h1>q: {{ item.quan_item }}</h1>
       <h1>name: {{ item.name_item }}</h1>
-      <img alt="error" :src="getImagePath(item.image_item)" />
+      <img alt="error" :src="'/images/' + item.image_item"/> />
     </div>
   </div>
 </template>
 
-<style scoped>
-
+<style >
+ img{
+   width: 200px;
+   height: 200px;
+ }
 </style>
