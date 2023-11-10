e3fd9cd269803803c41f802849677f965c427dfd
niktia <mr.nikita.zhuchenko@gmail.com>
2023-11-10 21:11:17 +0200

    fix

--- src/components/UiFormDBase.vue
+++ src/components/UiFormDBase.vue
@@ -12,7 +12,7 @@ const newProduct = ref({
 const addProduct = async () => {
   try {
     // Отправка данных на сервер
-    await axios.post('http://eseniabila.com.ua:3000/addProduct', newProduct.value);
+    await axios.post('https://eseniabila.com.ua/addProduct', newProduct.value);
 
     // Обновление списка продуктов после добавления
     // (вызов метода для обновления данных)
