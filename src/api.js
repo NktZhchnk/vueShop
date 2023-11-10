6dbb39180892262025f5f591a06e415b4591d215
niktia <mr.nikita.zhuchenko@gmail.com>
2023-11-10 18:17:52 +0200

    test post in mysql

--- src/api.js
+++ src/api.js
@@ -15,7 +15,12 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
+
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
 });
 
 app.post('/addProduct', (req, res) => {
