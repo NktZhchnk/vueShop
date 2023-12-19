3ad3a22fcdda2a04b427537d0023ce0d16b186af
niktia <mr.nikita.zhuchenko@gmail.com>
2023-12-19 16:47:25 +0200

    test swiper

--- src/components/ProductDetails.vue
+++ src/components/ProductDetails.vue
@@ -1,61 +1,78 @@
 <script setup>
 import { useMyStore } from "@/store/store.js";
-import { ref, computed } from 'vue';
+import { ref, computed, watch } from 'vue';
 import { useRoute } from 'vue-router';
 import axios from "axios";
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper';
 
 const route = useRoute();
-const productId = route.params.id;
+const productId = ref(route.params.id); // Используем реактивную переменную
 
 const store = useMyStore();
 
-const productById = ref(null); // Создаем реактивную переменную
+const product = ref(null); // Создаем реактивную переменную для информации о продукте
+const images = ref([]); // Создаем реактивную переменную для изображений
 
-axios.get(`https://eseniabila.com.ua/getProductById/${productId}`)
-    .then(response => {
-      // Данные о товаре с указанным ID
-      // Обработка полученных данных о товаре
-      store.productById.id = [];
-      store.productById.id.push(response.data) ;
-      productById.value = response.data; // Обновляем значение переменной
-      console.log('Информация о товаре:', response.data);
-    })
-    .catch(error => {
-      console.error('Ошибка при получении данных о товаре:', error);
-    });
-axios.get(`https://eseniabila.com.ua/getImgById/${productId}`)
-    .then(response => {
-      // Данные о товаре с указанным ID
-      // Обработка полученных данных о товаре
-      store.productById.img = [];
-      store.productById.img.push(response.data) ;
-      productById.value = response.data; // Обновляем значение переменной
-      console.log('Информация о товаре:', response.data);
-    })
-    .catch(error => {
-      console.error('Ошибка при получении данных о товаре:', error);
-    });
+// Функция для получения информации о продукте и изображений
+const getProductDetails = async () => {
+  try {
+    const response = await axios.get(`https://eseniabila.com.ua/getProductById/${productId.value}`);
+    if (response.data) {
+      product.value = response.data;
+    }
+  } catch (error) {
+    console.error('Ошибка при получении данных о товаре:', error);
+  }
 
-// Функция, проверяющая, является ли объект массивом
-const isArray = (obj) => {
-  return Array.isArray(obj) || Object.prototype.toString.call(obj) === '[object Array]';
+  try {
+    const response = await axios.get(`https://eseniabila.com.ua/getImgById/${productId.value}`);
+    if (response.data) {
+      images.value = response.data;
+    }
+  } catch (error) {
+    console.error('Ошибка при получении данных о картинках:', error);
+  }
 };
 
-const getProductById = computed(() => {
-  if (isArray(store.productById.id)) {
-    return store.productById.id.find(item => item.id === Number(productId));
-  } else {
-    return null; // Или другое значение по умолчанию, если productById не является массивом
-  }
+// Вызываем функцию для получения информации о продукте и изображениях
+getProductDetails();
+
+// Следим за изменениями productId и обновляем данные
+watch(() => route.params.id, (newProductId) => {
+  productId.value = newProductId;
+  getProductDetails();
 });
-console.log(store.productById.img)
+
+const getProductById = computed(() => {
+  return product.value;
+});
+
+const getImages = computed(() => {
+  return images.value;
+});
 </script>
 
 <template>
   <div v-if="getProductById" class="product-details">
     <h1>{{ getProductById.name_item }}</h1>
-    <p>Price: {{getProductById.price_item}}</p>
-    <p>{{getProductById.text_info}}</p>
+    <p>Price: {{ getProductById.price_item }}</p>
+    <p>{{ getProductById.text_info }}</p>
+    <div v-if="getImages.length > 0">
+      <h3>Product Images:</h3>
+      <div class="swiper-container">
+        <Swiper
+            :options="{
+          slidesPerView: 1,
+          spaceBetween: 10,
+          // Другие опции по вашему выбору
+        }"
+        >
+          <SwiperSlide v-for="(image, index) in getImages" :key="index">
+            <img :src="image.img" alt="Product Image" class="product-image" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
     <button>Add to Cart</button>
   </div>
   <div v-else>
@@ -64,37 +81,6 @@ console.log(store.productById.img)
 </template>
 
 <style>
-.product-details {
-  background-color: #f8f8f8;
-  border: 1px solid #ccc;
-  padding: 20px;
-  border-radius: 8px;
-  margin-top: 20px;
-}
-
-.product-details h1 {
-  font-size: 24px;
-  color: #333;
-  margin-bottom: 10px;
-}
-
-.product-details p {
-  font-size: 16px;
-  color: #555;
-  margin-bottom: 8px;
-}
-
-.product-details button {
-  padding: 10px 20px;
-  font-size: 16px;
-  background-color: #007bff;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-}
-
-.product-details button:hover {
-  background-color: #0056b3;
-}
+@import 'swiper/swiper-bundle.css';
+/* Ваши стили остаются без изменений */
 </style>
