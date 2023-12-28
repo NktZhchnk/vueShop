f09dc71b78ee64da5f20078227a9ebca84fc6244
niktia <mr.nikita.zhuchenko@gmail.com>
2023-12-28 15:06:46 +0200

    test ukr

--- src/components/Poshta/UiUkrPoshta.vue
+++ src/components/Poshta/UiUkrPoshta.vue
@@ -1,6 +1,6 @@
 <script setup>
 import axios from "axios";
-import {ref} from "vue"
+import {ref, onMounted} from "vue"
 let ukrRegion = ref(null);
 
 axios.get('/getUkrPoshtaData')
@@ -14,17 +14,15 @@ axios.get('/getUkrPoshtaData')
       console.error('Произошла ошибка при запросе на сервер UkrPoshta:', error);
     });
 
-axios.get('/getUkrPoshtaRegion')
-    .then(response => {
-      // Обробка отриманих даних з сервера
-      console.log('Ответ сервера UkrPoshtaRegion:', response.data);
-      ukrRegion.value = response.data
-      // Далі можна використовувати ці дані на фронтенді за необхідністю
-    })
-    .catch(error => {
-      // Обробка помилок
-      console.error('Произошла ошибка при запросе на сервер UkrPoshta:', error);
-    });
+onMounted(async () => {
+  try {
+    const response = await axios.get('/getUkrPoshtaRegion');
+    ukrRegion.value = response.data;
+    console.log('Ответ сервера UkrPoshtaRegion:', response.data);
+  } catch (error) {
+    console.error('Произошла ошибка при запросе на сервер UkrPoshta:', error);
+  }
+});
 
 console.log(ukrRegion.value)
 </script>
