5820be9a2129164ddbbadbbfaf4fc98b898a2f73
niktia <mr.nikita.zhuchenko@gmail.com>
2024-01-13 23:07:15 +0200

    fix orders

--- src/components/AdminPanel/UiOrders.vue
+++ src/components/AdminPanel/UiOrders.vue
@@ -10,9 +10,13 @@
       <!-- Ваш код для отображения карточек -->
       <div class="card">
         <h2>{{ order.first_name }} {{ order.last_name }}</h2>
-        <p>Order Date: {{ order.order_date }}</p>
-        <p>Telephone: {{ order.telephone }}</p>
-        <p>Comment: {{ order.comment }}</p>
+        <p>Дата: {{ order.order_date }}</p>
+        <p>Телефон: {{ order.telephone }}</p>
+        <p>Коментар: {{ order.comment }}</p>
+        <p>Город: {{ order.city }}</p>
+        <p v-if="order.address">Нова пошта: {{ order.address }}</p>
+        <p v-if="order.postal_code">УкрПошта: {{ order.postal_code }}</p>
+        <p>Total price: {{ order.total_price }} ₴.</p>
         <!-- Другие поля карточки -->
       </div>
     </router-link>
@@ -53,11 +57,38 @@ const uniqueOrders = computed(() => {
 /* Ваши стили для карточек */
 .card {
   border: 1px solid #ddd;
-  padding: 10px;
-  margin: 10px;
+  padding: 20px;
+  margin: 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  background-color: #fff;
+  transition: box-shadow 0.3s ease;
 }
-.link-order{
-  text-decoration: none; /* Убирает подчеркивание ссылки */
-  color: black; /* Задайте цвет текста, который вы хотите использовать */
+
+.card:hover {
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+}
+
+h2 {
+  font-size: 22px;
+  margin-bottom: 12px;
+  color: #333;
+}
+
+p {
+  font-size: 16px;
+  margin-bottom: 8px;
+  color: #555;
+}
+
+.link-order {
+  display: block;
+  text-decoration: none;
+  color: #3498db; /* Adjust the color to your preference */
+  transition: color 0.3s ease;
+}
+
+.link-order:hover {
+  color: #21618c; /* Darker shade on hover */
 }
 </style>
