b3138164517a5f7e0dd900a1b85b9a00d183a944
niktia <mr.nikita.zhuchenko@gmail.com>
2024-01-20 22:47:47 +0200

    test OrderDetails add logic rem Order

--- src/components/AdminPanel/OrderDetails.vue
+++ src/components/AdminPanel/OrderDetails.vue
@@ -3,7 +3,7 @@
 <template>
   <div class="order-details">
     <h2>Order Details</h2>
-    <svg @click.prevent="console.log('h')" xmlns="http://www.w3.org/2000/svg" height="28" width="26"
+    <svg @click.prevent="removeOrder()" xmlns="http://www.w3.org/2000/svg" height="28" width="26"
          viewBox="0 0 448 512">
       <path
           d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/>
@@ -51,6 +51,7 @@ import {useRoute} from "vue-router";
 const orderId = ref(null);
 const order = ref(null);
 
+
 onMounted(async () => {
   // Используем useRoute для получения доступа к $route
   const route = useRoute();
@@ -71,9 +72,19 @@ onMounted(async () => {
   }
 });
 
-const removeOrder = () => {
-
+const removeOrder = async () => {
+  for(const item of order.value){
+    if(item.order_product_id){
+      const productResponse = await  axios.get(`https://eseniabila.com.ua/getProductById${item.order_product_id}`);
+      console.log('varietyResponse', productResponse)
+    }
+    if(item.order_variety_id){
+      const varietyResponse = await  axios.get(`https://eseniabila.com.ua/getVarietiesById${item.order_variety_id}`);
+      console.log('varietyResponse', varietyResponse)
+    }
+  }
 }
+
 </script>
 
 <style scoped>
