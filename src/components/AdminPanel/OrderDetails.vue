2141276cf30ed36f74831c77ecb34b933f3e9dca
niktia <mr.nikita.zhuchenko@gmail.com>
2024-01-20 22:49:38 +0200

    test OrderDetails add logic rem Order v1.01

--- src/components/AdminPanel/OrderDetails.vue
+++ src/components/AdminPanel/OrderDetails.vue
@@ -76,11 +76,11 @@ const removeOrder = async () => {
   for(const item of order.value){
     if(item.order_product_id){
       const productResponse = await  axios.get(`https://eseniabila.com.ua/getProductById${item.order_product_id}`);
-      console.log('varietyResponse', productResponse)
+      console.log('varietyResponse', productResponse.data)
     }
     if(item.order_variety_id){
       const varietyResponse = await  axios.get(`https://eseniabila.com.ua/getVarietiesById${item.order_variety_id}`);
-      console.log('varietyResponse', varietyResponse)
+      console.log('varietyResponse', varietyResponse.data)
     }
   }
 }
