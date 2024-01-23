042982492083e1b1646a8fcbf277fa8706cb370c
niktia <mr.nikita.zhuchenko@gmail.com>
2024-01-23 23:47:08 +0200

    big update add editQuan

--- src/components/AdminPanel/UiEditProducts.vue
+++ src/components/AdminPanel/UiEditProducts.vue
@@ -9,45 +9,68 @@ onMounted(async () => {
   await store.fetchData();
 });
 
+
+
 let varietyQuantities = ref({});
+let qunProduct = ref(0)
 const getProductVarieties = (productId) => {
   return store.productVarieties.filter(variety => variety.product_id === productId)
 }
 const getTotalQuantity = (productId) => {
   const product = store.products.find(item => item.id === productId);
   if (product) {
-    return getProductVarieties(productId).reduce((total, variety) => total + variety.variety_quan, product.quan_item);
+    return getProductVarieties(productId).reduce((total, variety) => total, product.quan_item);
   }
 }
 const saveQuanProduct = async (productId, varietyId) => {
-  const values = Object.values(varietyQuantities.value)
-  const productQuantity = values.reduce((total, quantity) => total + quantity, 0)
 
-  const varietyResponse2 = await axios.put(
-      `https://eseniabila.com.ua/updateProductCount/${productId}`,
-      { variety_quan: productQuantity },
-      { headers: { 'Content-Type': 'application/json' } }
-  );
+  try {
+    if (varietyId.length !== 0) {
+      const values = Object.values(varietyQuantities.value);
+      const productQuantity = values.reduce((total, quantity) => total + quantity, 0);
 
+      // Запрос на обновление количества продукта
+      const varietyResponse2 = await axios.put(
+          `https://eseniabila.com.ua/updateProductCount/${productId}`,
+          {variety_quan: productQuantity},
+          {headers: {'Content-Type': 'application/json'}}
+      );
 
-  console.log('Ответ сервера количество вариации:', varietyResponse2.data);
+      console.log('Ответ сервера количество вариации:', varietyResponse2.data);
 
-  varietyId.forEach((variety) => {
+      // Запросы на обновление количества для каждой вариации
+      await Promise.all(varietyId.map(async (variety) => {
+        const varietyResponse = await axios.put(
+            `https://eseniabila.com.ua/updateVarietyCount/${variety.id}`,
+            {variety_quan: varietyQuantities.value[variety.id]},
+            {headers: {'Content-Type': 'application/json'}}
+        );
 
-    const varietyResponse = axios.put(`https://eseniabila.com.ua/updateVarietyCount/${variety.id}`, {
-      variety_quan: varietyQuantities.value[variety.id],
-    }, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+        console.log('Ответ сервера количество вариации:', varietyResponse.data);
+        console.log("Variety ID:", variety.id);
+        console.log("Variety Quantity:", variety.variety_quan);
+      }));
+    } else {
+      console.log('h', qunProduct.value);
 
-    console.log('Ответ сервера количество вариации:', varietyResponse.data);
+      // Запрос на обновление количества продукта в отсутствие вариаций
+      const varietyResponse2 = await axios.put(
+          `https://eseniabila.com.ua/updateProductCount/${productId}`,
+          {variety_quan: qunProduct.value},
+          {headers: {'Content-Type': 'application/json'}}
+      );
 
-    console.log("Variety ID:", variety.id);
-    console.log("Variety Quantity:", variety.variety_quan);
-  });
-}
+      console.log('Ответ сервера количество вариации:', varietyResponse2.data);
+    }
+  } catch (error) {
+    // Обработка ошибок
+    console.error('Произошла ошибка:', error);
+  } finally {
+    // Перезагрузка страницы вне зависимости от того, были ли ошибки или нет
+    window.location.reload();
+  }
+};
+
 </script>
 
 <template>
@@ -57,13 +80,13 @@ const saveQuanProduct = async (productId, varietyId) => {
       <ul class="product-info">
         <li>
           Общее количество: {{ getTotalQuantity(product.id) }}
-          <input v-show="false" v-model="qunProduct" />
+          <input v-if="getProductVarieties(product.id).length === 0" type="number" v-model="qunProduct"/>
         </li>
         <li>Вариации:</li>
         <ul class="varieties-list">
           <li v-for="variety in getProductVarieties(product.id)" :key="variety.id" class="variety-item">
             {{ variety.variety_name }} - {{ variety.variety_quan }}
-            <input type="number" v-model="varietyQuantities[variety.id]" />
+            <input type="number" v-model="varietyQuantities[variety.id]"/>
           </li>
           <button @click="saveQuanProduct(product.id, getProductVarieties(product.id))">сохранить</button>
         </ul>
@@ -105,11 +128,13 @@ input {
   padding: 0;
   margin-top: 5px;
 }
-.main-name{
+
+.main-name {
   word-wrap: break-word;
   height: 90px;
   overflow: hidden;
 }
+
 .variety-item {
   margin-bottom: 5px;
 }
