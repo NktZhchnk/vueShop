6ccd0112f9da6f07c2910fb01f542adb112ce0bc
niktia <mr.nikita.zhuchenko@gmail.com>
2024-01-25 23:11:14 +0200

    new style cart

--- src/components/UiCartItems.vue
+++ src/components/UiCartItems.vue
@@ -86,13 +86,27 @@ onMounted(loadCartProducts);
 
         <img :src="item.images.img" alt="Product Image" class="product-image">
         <div class="product-details">
-          <h1 class="text-name">{{ item.product.name_item }}</h1>
-          <p v-if="item.selectedVariety">Варіація: {{ item.selectedVariety.variety_name }}
-            <button @click="decrementQuanProduct(index)">-</button>
-            Кiл: {{ item.countProduct }}
-            <button @click="sumQuanProduct(index)">+</button>
+          <h1 class="text-name">{{ item.product.name_item }}
+            <svg @click="removeProduct(index)" class="icon-trash" xmlns="http://www.w3.org/2000/svg" height="28"
+                 width="26" viewBox="0 0 448 512">
+              <path
+                  d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"/>
+            </svg>
+          </h1>
+          <div v-if="item.selectedVariety">
+            <div>
+              <p>Варіація: {{ item.selectedVariety.variety_name }}</p>
+            </div>
+            <div>
+              <p>
+                <br>
+                <button @click="decrementQuanProduct(index)">-</button>
+                Кiл: {{ item.countProduct }}
+                <button @click="sumQuanProduct(index)">+</button>
+              </p>
+            </div>
+          </div>
 
-          </p>
 
           <div
               style="display: flex; height: 20px;align-items: center;"
@@ -104,22 +118,16 @@ onMounted(loadCartProducts);
           </div>
 
           <div style=" width: 100%; display: flex; justify-content: space-between; align-items: center">
-
             <p v-if="item.selectedVariety">Ціна: {{ item.selectedVariety.variety_price * item.countProduct }} ₴</p>
             <p v-if="item.selectedVariety === null">Ціна: {{ item.product.price_item * item.countProduct }} ₴</p>
 
-            <svg @click="removeProduct(index)" class="icon-trash" xmlns="http://www.w3.org/2000/svg" height="28"
-                 width="26" viewBox="0 0 448 512">
-              <path
-                  d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"/>
-            </svg>
           </div>
         </div>
       </div>
     </div>
     <div class="div-footer">
       <div class="cart-receipt">
-        <p>price:{{ allPriceCart }}</p>
+        <p>Загальна ціна: <span style="color: black">{{ allPriceCart }}</span> ₴</p>
         <router-link to="/uiConfirmationOrder">
           <button @click="store.swapShowPage()" class="button-green">Оформить Заказ</button>
         </router-link>
@@ -166,7 +174,9 @@ onMounted(loadCartProducts);
   word-wrap: break-word;
   height: 40px;
   overflow: hidden;
-
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
 }
 
 .div-header h1 {
@@ -176,29 +186,35 @@ onMounted(loadCartProducts);
 
 .div-body {
   padding: 20px;
-  display: flex;
   flex-wrap: wrap; /* Разрешаем перенос элементов на новую строку */
   overflow: auto; /* Добавляем прокрутку, если элементы не помещаются */
   gap: 20px;
 }
 
 .product-item {
-  width: calc(50% - 20px); /* Установка ширины элемента в половину контейнера с учетом отступов */
   margin-bottom: 20px; /* Отступ снизу между элементами */
-  border: 1px solid gray;
-  box-shadow: 3px 3px 5px gray;
-  border-radius: 20px;
+  width: 100%; /* Установка ширины элемента в половину контейнера с учетом отступов */
+  display: flex;
+  background: #f5f5f5;
+  border-radius: 10px;
+  border: 1px solid #cecece;
+  box-shadow: 2px 2px 10px gray;
 }
 
 .product-image {
-  width: 100%;
+  width: 250px;
   height: 250px;
   display: block;
-  border-radius: 20px 20px 0 0;
+  border-radius: 10px 2px 2px 10px;
+}
+
+img {
+  width: 200px;
 }
 
 .product-details {
   padding: 10px;
+  width: 100%;
 }
 
 .show-menu {
@@ -250,12 +266,18 @@ onMounted(loadCartProducts);
 @media (max-width: 450px) {
   .product-image {
     height: 200px;
+    width: 200px;
   }
 }
 
 @media (min-width: 800px) {
   .product-item {
-    width: calc(33% - 20px); /* Установка ширины элемента в половину контейнера с учетом отступов */
+    width: 100%; /* Установка ширины элемента в половину контейнера с учетом отступов */
+  }
+
+  .product-image {
+    height: 200px;
+    width: 300px;
   }
 }
 </style>
