8ff2cb530ca8f27320ce1151781c7daa53d714a1
niktia <mr.nikita.zhuchenko@gmail.com>
2024-02-03 00:10:12 +0200

    add info paymentMethod

--- src/LazyLoadImage.vue
+++ src/LazyLoadImage.vue
@@ -1,6 +1,6 @@
 <template>
   <div ref="intersectionTarget" class="lazy-load">
-    <img v-if="isVisible" :src="src" :alt="alt" />
+    <img v-if="isVisible" :src="src" :alt="alt"/>
   </div>
 </template>
 
@@ -55,11 +55,11 @@ export default {
   height: 100%;
   position: relative;
   overflow: hidden;
+}
 
-  img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
+img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 }
 </style>
