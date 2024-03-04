4a194ad824d680da685b872650051ba01359e6ac
niktia <mr.nikita.zhuchenko@gmail.com>
2024-03-04 12:40:51 +0200

    new logic users DetailsOrderUser

--- src/components/Users/UiDetailsOrdersUsers.vue
+++ src/components/Users/UiDetailsOrdersUsers.vue
@@ -1,11 +1,32 @@
 <script setup>
+import {onMounted, ref} from "vue";
+import axios from "axios";
 
+const phoneNumber = ref(localStorage.getItem('phoneNumber'));
+const userLogin = ref(localStorage.getItem('userLogin'));
+const firstName = ref(localStorage.getItem('firstName'));
+const lastName = ref(localStorage.getItem('lastName'));
+
+const ordersData = ref([]);
+
+const fetchOrders = async () => {
+  try {
+    const response = await axios.get(`/getOrdersPhone?phoneNumber=${phoneNumber.value}`);
+    ordersData.value = response.data
+  } catch (error) {
+    console.log('error Запроса:', error)
+  }
+}
+
+onMounted(() => {
+  fetchOrders()
+})
 </script>
 
 <template>
   <div class="center-container">
     <div class="main-detail">
-
+      <button @click="fetchOrders">424242</button>
     </div>
   </div>
 </template>
