1fdfabee27e3a4618dad71997e885896019524de
niktia <mr.nikita.zhuchenko@gmail.com>
2024-03-10 22:54:04 +0200

    test

--- src/api.js
+++ src/api.js
@@ -34,7 +34,7 @@ function backupMySQL() {
     const dbName = 'products';
     const backupPath = '/var/www/';
 
-    const command = `mysqldump --defaults-extra-file=/root/my.cnf -h ${connection.config.host} -u ${connection.config.user} ${dbName} > ${backupPath}/${dbName}_backup.sql`;
+    const command = `mysqldump --defaults-extra-file=/root/my.cnf ${dbName} > ${backupPath}/${dbName}_backup.sql`;
 
     exec(command, (error, stdout, stderr) => {
         if (error) {
@@ -51,7 +51,7 @@ setInterval(() => {
     const hours = now.getHours();
     const minutes = now.getMinutes();
 
-    if (hours === 22 && minutes === 50) {
+    if (hours === 22 && minutes === 57) {
         backupMySQL();
     }
 }, 60000); // Проверка каждую минуту
